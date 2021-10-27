b2e5540cb3b27aa2e26d4e7edf76b431a0e66a6c
shashankg11 <shashank11gupta22@gmail.com>
2021-10-27 15:41:28 +0530

    variables done

---
+++ script.js
@@ -0,0 +1,6 @@
+const billInput=document.getElementById("bill");
+const tipInput=document.getElementById("tip");
+const minusBtn=document.getElementById("minusBtn");
+const plusBtn=document.getElementById("plusBtn");
+const peopleInput=document.getElementById("ppl");
+console.log(plusBtn.value);
